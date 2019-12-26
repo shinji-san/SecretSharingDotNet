93d4bbf423adb80589aaab63c839cb92b438653e
Sebastian Walther <github.dev@sebastian-walther.de>
2019-12-26 03:17:21 +0100

    FinitePoint.cs: Add 'ToByteArray' method
    Add private method 'ToByteArray' which will be used by
    the parser to deserialize the FinitePoint structure.
    
    Resolves: No entry

--- src/Cryptography/FinitePoint.cs
+++ src/Cryptography/FinitePoint.cs
@@ -200,5 +200,48 @@ namespace SecretSharingDotNet.Cryptography
 
             return accum;
         }
+
+        /// <summary>
+        /// Converts a hexadecimal string to a byte array.
+        /// </summary>
+        /// <param name="hextString">hexadecimal string</param>
+        /// <returns></returns>
+        private static byte[] ToByteArray(string hextString)
+        {
+            byte[] bytes = new byte[hextString.Length / 2];
+            var hexValues = Array.AsReadOnly(new[] {
+                0x00,
+                0x01,
+                0x02,
+                0x03,
+                0x04,
+                0x05,
+                0x06,
+                0x07,
+                0x08,
+                0x09,
+                0x00,
+                0x00,
+                0x00,
+                0x00,
+                0x00,
+                0x00,
+                0x00,
+                0x0A,
+                0x0B,
+                0x0C,
+                0x0D,
+                0x0E,
+                0x0F
+            });
+
+            for (int x = 0, i = 0; i < hextString.Length; i += 2, x += 1)
+            {
+                const char ZeroDigit = '0';
+                bytes[x] = (byte)((hexValues[char.ToUpper(hextString[i + 0]) - ZeroDigit] << 4) | hexValues[char.ToUpper(hextString[i + 1]) - ZeroDigit]);
+            }
+
+            return bytes;
+        }
     }
 }
