94a073e8570230cc2aa69b21b546c29fe7573f59
Sebastian Walther <github.dev@sebastian-walther.de>
2020-12-24 14:27:50 +0100

    ExtendedGcdResult.cs: Fix potential code quality issues
    Resolves: No entry

--- src/Math/ExtendedGcdResult.cs
+++ src/Math/ExtendedGcdResult.cs
@@ -17,20 +17,19 @@ namespace SecretSharingDotNet.Math
     /// Represents the result of the extended greatest common divisor computation.
     /// </summary>
     /// <typeparam name="TNumber">Numeric data type</typeparam>
-    public struct ExtendedGcdResult<TNumber> : IEquatable<ExtendedGcdResult<TNumber>>
+    public readonly struct ExtendedGcdResult<TNumber> : IEquatable<ExtendedGcdResult<TNumber>>
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="ExtendedGcdResult{TNumber}"/> struct.
         /// </summary>
         /// <param name="gcd">Greatest common divisor</param>
-        /// <param name="coeff">Bézout coefficients</param>
-        /// <param name="quot">Quotients by the gcd</param>
-        public ExtendedGcdResult (Calculator<TNumber> gcd, IList<Calculator<TNumber>> coeff, IList<Calculator<TNumber>> quot)
+        /// <param name="coefficients">Bézout coefficients</param>
+        /// <param name="quotients">Quotients by the gcd</param>
+        public ExtendedGcdResult(Calculator<TNumber> gcd, IList<Calculator<TNumber>> coefficients, IList<Calculator<TNumber>> quotients)
         {
-            this.GreatestCommonDivisor = gcd ??
-                throw new System.ArgumentNullException (nameof (gcd));
-            this.BezoutCoefficients = new ReadOnlyCollection<Calculator<TNumber>> (coeff);
-            this.Quotients = new ReadOnlyCollection<Calculator<TNumber>> (quot);
+            this.GreatestCommonDivisor = gcd ?? throw new ArgumentNullException(nameof(gcd));
+            this.BezoutCoefficients = new ReadOnlyCollection<Calculator<TNumber>>(coefficients);
+            this.Quotients = new ReadOnlyCollection<Calculator<TNumber>>(quotients);
         }
 
         /// <summary>
@@ -49,50 +48,55 @@ namespace SecretSharingDotNet.Math
         public ReadOnlyCollection<Calculator<TNumber>> Quotients { get; }
 
         /// <summary>
+        /// Equality operator
+        /// </summary>
+        /// <param name="left">The left operand</param>
+        /// <param name="right">The right operand</param>
+        /// <returns>Returns <see langword="true"/> if its operands are equal, otherwise <see langword="false"/>.</returns>
+        public static bool operator ==(ExtendedGcdResult<TNumber> left, ExtendedGcdResult<TNumber> right) => left.Equals(right);
+
+        /// <summary>
+        /// Inequality operator
+        /// </summary>
+        /// <param name="left">The left operand</param>
+        /// <param name="right">The right operand</param>
+        /// <returns>Returns <see langword="true"/> if its operands are not equal, otherwise <see langword="false"/>.</returns>
+        public static bool operator !=(ExtendedGcdResult<TNumber> left, ExtendedGcdResult<TNumber> right) => !left.Equals(right);
+
+        /// <summary>
         /// Determines whether this structure and an<paramref name="other"/> specified <see cref="ExtendedGcdResult{TNumber}"/> structure have the same value.
         /// </summary>
         /// <param name="other">The <see cref="ExtendedGcdResult{TNumber}"/> structure to compare</param>
-        /// <returns><c>true</c> if the value of the <paramref name="other"/> parameter is the same as the value of this structure; otherwise <c>false</c>.</returns>
-        public bool Equals (ExtendedGcdResult<TNumber> other) => this.Quotients.SequenceEqual (other.Quotients) &&
-        this.BezoutCoefficients.SequenceEqual (other.BezoutCoefficients) &&
-        this.GreatestCommonDivisor == this.GreatestCommonDivisor;
+        /// <returns><see langword="true"/> if the value of the <paramref name="other"/> parameter is the same as the value of this structure; otherwise <see langword="false"/>.</returns>
+        public bool Equals(ExtendedGcdResult<TNumber> other) => this.Quotients.SequenceEqual(other.Quotients) &&
+        this.BezoutCoefficients.SequenceEqual(other.BezoutCoefficients) &&
+        this.GreatestCommonDivisor == other.GreatestCommonDivisor;
 
         /// <summary>
         /// Determines whether the specified object is equal to the current <see cref="ExtendedGcdResult{TNumber}"/> structure.
         /// </summary>
         /// <param name="obj">The object to compare with the current <see cref="ExtendedGcdResult{TNumber}"/> structure.</param>
-        /// <returns><c>true</c> if the specified object is equal to the current <see cref="ExtendedGcdResult{TNumber}"/> structure; otherwise <c>false</c>.</returns>
-        public override bool Equals (object obj)
+        /// <returns><see langword="true"/> if the specified object is equal to the current <see cref="ExtendedGcdResult{TNumber}"/> structure; otherwise <see langword="false"/>.</returns>
+        public override bool Equals(object obj)
         {
-            if (obj == null)
-            {
-                return false;
-            }
-
-            return this.Equals ((ExtendedGcdResult<TNumber>) obj);
+            return obj != null && this.Equals((ExtendedGcdResult<TNumber>)obj);
         }
 
         /// <summary>
         /// Returns the hash code for the current <see cref="ExtendedGcdResult{TNumber}"/> structure.
         /// </summary>
         /// <returns>A 32-bit signed integer hash code.</returns>
-        public override int GetHashCode ()
+        public override int GetHashCode()
         {
-            int hashCode = this.GreatestCommonDivisor.GetHashCode ();
+            int hashCode = this.GreatestCommonDivisor.GetHashCode();
             if (this.BezoutCoefficients != null)
             {
-                foreach (var coefficient in this.BezoutCoefficients)
-                {
-                    hashCode ^= coefficient.GetHashCode ();
-                }
+                hashCode = this.BezoutCoefficients.Aggregate(hashCode, (current, coefficient) => current ^ coefficient.GetHashCode());
             }
 
             if (this.Quotients != null)
             {
-                foreach (var quotient in this.Quotients)
-                {
-                    hashCode ^= quotient.GetHashCode ();
-                }
+                hashCode = this.Quotients.Aggregate(hashCode, (current, quotient) => current ^ quotient.GetHashCode());
             }
 
             return hashCode;
