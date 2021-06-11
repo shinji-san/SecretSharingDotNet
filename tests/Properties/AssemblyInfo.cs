630b9382c63a1f28c73e43ac113a026c665a18f4
Sebastian Walther <github.dev@sebastian-walther.de>
2021-06-12 00:34:49 +0200

    AssemblyInfo.cs: Disable CLS compliance
    Disable CLS compliance for theory tests to use
    -array
    -collection
    -Enumerable
    -etc.
    of test data as attribute arguments.
    
    For details see:
    https://docs.microsoft.com/en-us/dotnet/csharp/misc/cs3016?f1url=%3FappId%3Droslyn%26k%3Dk(CS3016)
    
    Resolves: #14

--- tests/Properties/AssemblyInfo.cs
+++ tests/Properties/AssemblyInfo.cs
@@ -19,4 +19,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: System.CLSCompliant(true)]
+[assembly: System.CLSCompliant(false)]
