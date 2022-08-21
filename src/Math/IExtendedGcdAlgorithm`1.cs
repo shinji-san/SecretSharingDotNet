ab23ee37b8c809005f6749f520f879cc3e550460
Sebastian Walther <github.dev@sebastian-walther.de>
2022-08-21 02:12:36 +0200

    IExtendedGcdAlgorithm.cs: Refactored
    Split into 2 generic classes for legacy and decoupling
    purposes.
    
    Resolves: No entry

---
+++ src/Math/IExtendedGcdAlgorithm`1.cs
@@ -0,0 +1,36 @@
+// ----------------------------------------------------------------------------
+// <copyright file="IExtendedGcdAlgorithm`1.cs" company="Private">
+// Copyright (c) 2022 All Rights Reserved
+// </copyright>
+// <author>Sebastian Walther</author>
+// <date>08/20/2022 02:31:37 PM</date>
+// ----------------------------------------------------------------------------
+
+#region License
+// ----------------------------------------------------------------------------
+// Copyright 2022 Sebastian Walther
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+#endregion
+
+namespace SecretSharingDotNet.Math
+{
+    /// <inheritdoc />
+    public interface IExtendedGcdAlgorithm<TNumber> : IExtendedGcdAlgorithm<TNumber, ExtendedGcdResult<TNumber>> { }
+}
