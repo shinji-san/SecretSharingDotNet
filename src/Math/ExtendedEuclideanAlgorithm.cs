cade7ddcf27c4e3496d6d75bdc92df096654193e
Sebastian Walther <github.dev@sebastian-walther.de>
2022-09-21 22:20:45 +0200

    ExtendedEuclideanAlgorithm.cs: Fix style guide violations
    Resolves: No entry

--- src/Math/ExtendedEuclideanAlgorithm.cs
+++ src/Math/ExtendedEuclideanAlgorithm.cs
@@ -6,6 +6,29 @@
 // <date>04/20/2019 10:52:28 PM</date>
 // ----------------------------------------------------------------------------
 
+#region License
+// ----------------------------------------------------------------------------
+// Copyright 2022 Sebastian Walther
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+#endregion
+
 namespace SecretSharingDotNet.Math
 {
     /// <summary>
@@ -52,9 +75,9 @@ namespace SecretSharingDotNet.Math
                 }
             }
 
-            var coefficient = new[] { lastX, lastY };
-            var quot = new[] { x, y };
-            return new ExtendedGcdResult<TNumber>(lastR, coefficient, quot);
+            var coefficients = new[] {lastX, lastY};
+            var quotients = new[] {x, y};
+            return new ExtendedGcdResult<TNumber>(lastR, coefficients, quotients);
         }
     }
 }
