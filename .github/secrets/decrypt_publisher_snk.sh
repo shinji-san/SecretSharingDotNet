5d4d1d429b2e12453d36e873c8bd16b7a2286542
Sebastian Walther <github.dev@sebastian-walther.de>
2023-12-30 18:59:54 +0100

    decrypt_publisher_snk.sh: Remove pub key id replacement
    Resolves: No entry

--- .github/secrets/decrypt_publisher_snk.sh
+++ .github/secrets/decrypt_publisher_snk.sh
@@ -4,4 +4,3 @@ rm -f ${CDIR}/../../src/SecretSharingDotNet.snk
 # Decrypt the file
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$PUBLISHER_SNK" --output ${CDIR}/../../src/SecretSharingDotNet.snk ${CDIR}/SecretSharingDotNetPublisher.snk.gpg
-sed -i -r "s/(.*=)([0-9a-f]+)(.*)/\1$PUBLISHER_PUB_KEY\3/g" ${CDIR}/../../src/Cryptography/Secret.cs
